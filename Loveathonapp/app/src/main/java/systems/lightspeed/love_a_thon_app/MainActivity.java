5c07897ea6810f44a0b11474012df6ea2ae7da6b
ThomasNakagawa <tnakagaw@sfu.ca>
2018-02-10 11:19:02 -0800

    Add some basic ui

--- Loveathonapp/app/src/main/java/systems/lightspeed/love_a_thon_app/MainActivity.java
+++ Loveathonapp/app/src/main/java/systems/lightspeed/love_a_thon_app/MainActivity.java
@@ -2,6 +2,8 @@ package systems.lightspeed.love_a_thon_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("Hello world");
     }
 }
