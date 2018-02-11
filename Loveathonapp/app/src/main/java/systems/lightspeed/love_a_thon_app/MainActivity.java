7c75214e07a3182145ab61030d9695b32e9fbb4c
Yagnik Vadher <yagniktechmail@gmail.com>
2018-02-10 19:49:56 -0800

    Added bacground process on sepereate thread to communicate with arduino

--- Loveathonapp/app/src/main/java/systems/lightspeed/love_a_thon_app/MainActivity.java
+++ Loveathonapp/app/src/main/java/systems/lightspeed/love_a_thon_app/MainActivity.java
@@ -38,6 +38,33 @@ import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
+
+
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.os.Handler;
+
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     TextView mText;
@@ -54,6 +81,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     //Firebase Work
     DatabaseReference mDatabaseLocationDetails;
     DatabaseReference partnerLocationDetails;
+
+    //Arduino
+    Handler handler = new Handler();
+    int delay = 5000; //milliseconds
+    int counter = 0;
+    public final String ACTION_USB_PERMISSION = "systems.lightspeed.love_a_thon_app.USB_PERMISSION";
+
+    UsbManager usbManager;
+    UsbDevice device;
+    UsbSerialDevice serialPort;
+    UsbDeviceConnection connection;
 
     private void listenForPartner() {
         partnerLocationDetails = FirebaseDatabase.getInstance().getReference().child("Direction_Details").child(partnerName);
@@ -95,7 +133,138 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         Sensor mySensors = mySensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
         mySensorManager.registerListener(this, mySensors, SensorManager.SENSOR_DELAY_NORMAL);
         listenForPartner();
+
+
+        usbManager = (UsbManager) getSystemService(this.USB_SERVICE);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(broadcastReceiver, filter);
+
     }
+
+    UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() { //Defining a Callback which triggers whenever data is read.
+        @Override
+        public void onReceivedData(byte[] arg0) {
+            String data = null;
+            try {
+                data = new String(arg0, "UTF-8");
+                data.concat("/n");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+        }
+    };
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() { //Broadcast Receiver to automatically start and stop the Serial connection.
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
+                boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
+                if (granted) {
+                    connection = usbManager.openDevice(device);
+                    serialPort = UsbSerialDevice.createUsbSerialDevice(device, connection);
+                    if (serialPort != null) {
+                        if (serialPort.open()) { //Set Serial Connection Parameters.
+
+                            serialPort.setBaudRate(9600);
+                            serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                            serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                            serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                            serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                            serialPort.read(mCallback);
+
+                        } else {
+                            Log.d("SERIAL", "PORT NOT OPEN");
+                        }
+                    } else {
+                        Log.d("SERIAL", "PORT IS NULL");
+                    }
+                } else {
+                    Log.d("SERIAL", "PERM NOT GRANTED");
+                }
+            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)) {
+                onClickStart();
+            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
+                onClickStop();
+            }
+        }
+
+        ;
+    };
+
+
+
+    public void onClickStart() {
+
+        HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
+        if (!usbDevices.isEmpty()) {
+            boolean keep = true;
+            for (Map.Entry<String, UsbDevice> entry : usbDevices.entrySet()) {
+                device = entry.getValue();
+                int deviceVID = device.getVendorId();
+                if (deviceVID == 0x1A86)//Arduino Vendor ID
+                {
+                    PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+                    usbManager.requestPermission(device, pi);
+                    keep = false;
+                } else {
+                    connection = null;
+                    device = null;
+                }
+
+                if (!keep)
+                    break;
+            }
+        }
+        handler.postDelayed(new Runnable(){
+            public void run(){
+
+                if (percent > 0 && percent < 80){
+                    if(counter >= 3){
+                        counter = 3;
+                    }else {
+                        counter++;
+                    }
+
+                }else{
+                    if (counter <= 0){
+                        counter = 0;
+                    }else {
+                        counter--;
+                    }
+
+                }
+
+
+                if(counter == 0){
+                    onClickSend("1");
+                }else if(counter == 1){
+                    onClickSend("2");
+                }else if (counter == 2){
+                    onClickSend("3");
+                }
+
+                handler.postDelayed(this, delay);
+            }
+        }, delay);
+
+
+    }
+
+    public void onClickSend(String data) {
+        String string = data;
+        serialPort.write(string.getBytes());
+
+    }
+
+
+    public void onClickStop() {
+        serialPort.close();
+    }
+
     double distance(double srcLat, double srcLng, double desLat, double desLng) {
         double earthRadius = 3958.75;
         double dLat = Math.toRadians(desLat - srcLat);
